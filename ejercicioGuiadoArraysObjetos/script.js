888ca0a27d413e13f336b73c0755b5029536912f
taniaBob <akibonorulz@gmail.com>
2024-07-30 13:26:55 -0400

    ok

---
+++ ejercicioGuiadoArraysObjetos/script.js
@@ -0,0 +1,45 @@
+// Requerimientos
+// 1. Crear la estructura de datos mediante arreglos y objetos. Debe crear 1 arreglo por cada listado (Radiología, Traumatología y Dental) y 1 objeto por cada fila de información que contengan los listados, considerando las propiedades establecidas en la descripción (5 Puntos). 
+
+let arrayDePacientes = [
+
+    {   hora: "11.00",
+        especialista: "Ignacio Schultz",
+        paciente: "Francisca Rojas",
+        rut: "9878782-1",
+        prevision: "FONASA" 
+    },
+
+    {   hora: "11.30",
+        especialista: "Federico Subercaseaux",
+        paciente: "Pamela Estrada",
+        rut: "15345241-3",
+        prevision: "ISAPRE" 
+    },
+
+    {   hora: "15.00",
+        especialista: "Fernando Wurthz",
+        paciente: "Armando Luna",
+        rut: "16445345-9",
+        prevision: "ISAPRE" 
+    },
+
+    {   hora: "15.30",
+        especialista: "Ana Maria Godoy",
+        paciente: "Manuel Godoy",
+        rut: "17666419-0",
+        prevision: "FONASA" 
+    },
+
+    {   hora: "16.00",
+        especialista: "Patricia Suazo",
+        paciente: "Ramon Ulloa",
+        rut: "",
+        prevision: "" 
+    }
+]
+
+
+// 2. Mostrar por pantalla la primera y última atención de cada listado, desplegando el nombre del paciente junto con la previsión, separados por un guión (utilizar índices de arreglos para esto) (3 Puntos). Ej: Primera atención: Juan Pérez - Fonasa | Última atención: Ana Gálvez - Isapre.
+
+// 3. Recorrer el arreglo y mostrar su contenido en una tabla (2 Puntos). 
---
+++ metodosDeArreglos/index2.js
@@ -0,0 +1,8 @@
+// metodos de arreglos
+  
+
+// push agrega al final
+// pop elimina el ultimo elemento del array
+// forEach recorre todo un array y lo muestra
+// map hace un array recorre un array y hace uno nuevo con algun cambio que se le haya indicado en la funcion
+// filter filtra duh  
